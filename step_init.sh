13112ea628bd9899be9f6b6ae09905cb252b30a9
Eli <61595010+elish266@users.noreply.github.com>
2023-07-09 13:44:04 +0300

    Add files via upload

--- step_init.sh
+++ step_init.sh
@@ -50,6 +50,7 @@ if [[ -n $APPDOME_BRANCH_ANDROID ]]; then
     branch=$APPDOME_BRANCH_ANDROID
 fi
 
+echo "Running Branch: $branch"
 # step execusion
 git clone --branch $branch https://github.com/Appdome/bitrise-step-appdome-build-2secure-android.git  > /dev/null
 cd bitrise-step-appdome-build-2secure-android
